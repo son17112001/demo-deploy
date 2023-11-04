897e62daaec87a7cd5af5d619488fb13997d34e2
Nguyen Van Son <son17112001@gmail.com>
2023-11-04 11:50:01 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div id="particles-js"></div>
+          <div class="content">
+          <h1>Welcome to Demo Deploy Github Action</h1>
+          <p>Exploring the universe, one launch at a time.</p>
+          <button class="spacex-button">Explore</button>
+          </div>
+    </div>
+  );
+}
+
+export default App;
